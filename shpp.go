84c2df5187fc1b5b144293ccb00bfc98ba03f990
Vladyslav Aviedov <vladaviedov@protonmail.com>
2024-09-18 18:32:12 -0400

    input stream and fixes

--- shpp.go
+++ shpp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/jessevdk/go-flags"
@@ -16,10 +17,10 @@ var opts struct {
 }
 
 func main() {
-	parser := flags.NewParser(&opts, flags.Default ^ flags.HelpFlag)
+	parser := flags.NewParser(&opts, flags.Default ^ flags.HelpFlag ^ flags.PrintErrors)
 	args, err := parser.Parse()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to parse arguments: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "failed to parse arguments: %s\n", err.Error())
 		os.Exit(2)
 	}
 
@@ -34,24 +35,37 @@ func main() {
 	} else {
 		outStream, err = os.Create(opts.Output)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to open output file: %s", err.Error())
+			fmt.Fprintf(os.Stderr, "failed to open output file: %s\n", err.Error())
 			os.Exit(1)
 		}
 	}
+	defer outStream.Close()
 
-	if len(args) != 1 || (opts.Stdin && len(args) != 0) {
+	if (!opts.Stdin && len(args) != 1) || (opts.Stdin && len(args) != 0) {
 		usage(os.Stderr)
 		os.Exit(2)
 	}
 
-	outStream.Close()
+	var inStream *os.File = nil	
+	if opts.Stdin {
+		inStream = os.Stdin
+	} else {
+		inStream, err = os.Open(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open source file: %s\n", err.Error())
+		}
+	}
+	defer inStream.Close()
+
+	data, _ := io.ReadAll(inStream)
+	outStream.Write(data)
 }
 
 func usage(toFile *os.File) {
 	fmt.Fprintf(toFile, "usage: %s [options] <source>\n", os.Args[0])
 	fmt.Fprintf(toFile, "\n")
-	fmt.Fprintf(toFile, "%-20s - %s\n", "-o, --output <file>", "Set output file")
 	fmt.Fprintf(toFile, "%-20s - %s\n", "-x, --stdin", "Read input file from stdin (source should be left empty)")
 	fmt.Fprintf(toFile, "%-20s - %s\n", "-h, --help", "Show usage information")
 	fmt.Fprintf(toFile, "%-20s - %s\n", "-v, --version", "Show program version")
+	fmt.Fprintf(toFile, "%-20s - %s\n", "-o, --output <file>", "Set output file")
 }
